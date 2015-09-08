0f7e3aaf380933a4ddca74e815a3fe1ca8ecfdf2
sebastian.bogado <sebastian.bogado@fdvsolutions.com>
2015-09-08 04:14:11 -0300

    Hangouts client: get user info from ids

--- server/components/hangouts.js
+++ server/components/hangouts.js
@@ -7,7 +7,8 @@ var _ = require('underscore');
 // requested token.
 var creds = function() {
   return {
-    auth: process.env.HANGOUTS_TOKEN
+    auth: _.constant(process.env.HANGOUTS_TOKEN)
+    //auth: Client.authStdin
   };
 };
 
@@ -19,23 +20,46 @@ client.loglevel('debug');
 // receive chat message events
 client.on('chat_message', function(ev) {
   var convId = ev.conversation_id.id;
-  var userId = ev.sender_id.gaia_id;
+  var userId = ev.sender_id.chat_id;
   var chatMessage = _.pluck(ev.chat_message.message_content.segment, 'text').join('');
   var timestamp = ev.timestamp;
-  console.log(ev);
-  console.log('convId: ', convId);
-  console.log('userId: ', userId);
-  console.log('timestamp: ', timestamp);
-  console.log('chatMessage: ', chatMessage);
-  return;
+  //console.log(ev);
+  client.getentitybyid([userId])
+    .then( function (response) {
+      var userProperties = response.entities[0].properties;
+      var userName = userProperties.display_name;
+      var photoUrl = userProperties.photo_url;
+      console.log('convId: ', convId);
+      console.log('userId: ', userId, ', name: ', userName);
+      console.log('timestamp: ', timestamp);
+      console.log('chatMessage: ', chatMessage);
+    })
+    .fail(function (err) {
+      console.error(err);
+    })
+    .done();
 });
 
-// connect and post a message.
-// the id is a conversation id.
-client.connect(creds).then(function() {
-  return client.sendchatmessage('UgyGaOKODAq2G21X2QV4AaABAQ',
-    [[0, 'Hello World']]);
-}).done();
+
+var reconnect = function() {
+  client.connect(creds).then(function() {
+    //return client.sendchatmessage(process.env.HANGOUTS_CONV_ID,
+    return client.sendchatmessage('UgyU8IOOS2uslw1tjV54AaABAQ',
+      [[0, 'gg']]);
+  }).done();
+};
+
+// whenever it fails, we try again
+client.on('connect_failed', function() {
+  Q.Promise(function(rs) {
+    console.info('connect_failed event. Retrying in 60 seconds');
+    // backoff for 60 seconds
+    setTimeout(rs, 60000);
+  }).then(reconnect);
+});
+
+// start connection
+reconnect();
 
 // Expose client
 exports = module.exports = client;
