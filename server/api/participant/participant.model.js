d2d2dae00030cb6e81afa8cd77073e8abcf7f851
sebastian.bogado <sebastian.j.bogado@gmail.com>
2015-09-10 04:20:51 -0300

    Adds default URL to participant model

--- server/api/participant/participant.model.js
+++ server/api/participant/participant.model.js
@@ -3,10 +3,12 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
+var GOOGLE_DEFAULT_PHOTO_URL = 'https://lh6.googleusercontent.com/-Q9v7vFhQOvo/AAAAAAAAAAI/AAAAAAAAAAA/f8l6Kow78cg/s32-c/photo.jpg';
+
 var ParticipantSchema = new Schema({
   firstName: String,
   displayName: String,
-  photoUrl: String,
+  photoUrl: { type: String, default: GOOGLE_DEFAULT_PHOTO_URL },
   totalMessages: { type: Number, default: 0 },
   hangoutsUserId: { type: String, index: { unique: true } }
 });
