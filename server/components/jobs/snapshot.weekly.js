2791f4bcdb3c2055997ce2d484ed030b6b594015
sebastian.bogado <sebastian.j.bogado@gmail.com>
2015-09-20 20:12:25 -0300

    Ranking snapshots: weekly, monthly and yearly snapshots

---
+++ server/components/jobs/snapshot.weekly.js
@@ -0,0 +1,76 @@
+'use strict';
+
+var Q = require('q');
+var _ = require('lodash');
+var moment = require('moment');
+
+var Message = require('../../api/message/message.model');
+var Participant = require('../../api/participant/participant.model');
+var Snapshot = require('../../api/snapshot/snapshot.model');
+
+Participant.findQ = Q.nbind(Participant.find, Participant);
+
+function populateResultsWithParticipants(results) {
+  return function (participants) {
+    var populatedResults = [];
+
+    _.each(results, function (result) {
+      var participant = _.findWhere(participants, {_id: result._id});
+
+      if (!participant) return console.warn("Participant with id %s not found, but has messages", result._id);
+
+      populatedResults.push({
+        particinantId: participant._id,
+        photoUrl: participant.photoUrl,
+        displayName: participant.displayName,
+        totalMessages: result.totalMessages
+      });
+    });
+
+    return populatedResults;
+  }
+}
+
+module.exports = function(cron) {
+  var to = cron.nextRun;
+  var from = moment(to).subtract(1, 'week').toDate();
+
+  var def = Q.defer();
+
+  // Model.exec already returns a promise but its API differs from kriskowal's
+  Message
+    .aggregate()
+    .match( {timestamp: { $lt: to, $gte: from}})
+    .project( 'participantId -_id')
+    .group({_id: "$participantId", totalMessages: {$sum: 1}})
+    .allowDiskUse(true)
+    .exec( function execAggregation(err, results) {
+      if (err) return def.reject(err);
+
+      var participants = _.pluck(results, '_id');
+
+      return Participant.findQ({_id: {$in: participants}}, 'displayName photoUrl', {lean: true})
+        .then(populateResultsWithParticipants(results))
+        .then( function saveSnapshot(results) {
+
+          var snapshot = new Snapshot({
+            type: 'weekly',
+            timespan: {
+              from: from,
+              to: to
+            },
+            ranking: _.sortBy(results, 'totalMessages').reverse()
+          });
+
+          return Q.nfcall(snapshot.save)
+            .then(_.constant(snapshot));
+        })
+        .then( function logStuff(snapshot) {
+          console.log("Snapshot type %s from %s to %s saved.", snapshot.type, snapshot.timespan.from, snapshot.timespan.to);
+          def.resolve();
+        })
+        .fail(def.reject);
+    });
+
+  return def.promise;
+};
