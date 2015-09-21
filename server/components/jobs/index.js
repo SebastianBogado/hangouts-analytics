60e2b3efc0fa3b7375534b3df4f7eb48577f961d
sebastian.bogado <sebastian.j.bogado@gmail.com>
2015-09-21 09:35:05 -0300

    Ranking snapshots: adds cron seeds and crontab interface

--- server/components/jobs/index.js
+++ server/components/jobs/index.js
@@ -4,43 +4,41 @@ var CronJob = require('cron').CronJob;
 var _ = require('lodash');
 var Q = require('q');
 
+var Cron = require('../../api/cron/cron.model');
+
+Cron.findQ = Q.nbind(Cron.find, Cron);
+
 var app;
+
+// Singleton. Checks existent crons on start
+var cronTab = {
+  runCron: runCron,
+  runExistentCrons: runExistentCrons,
+  getCronStatus: getCronStatus
+};
+
 var TZ = process.env.TZ  || 'America/Argentina/Buenos_Aires';
 
-var crons = [{
-  name: 'dailyMessages',
-  description: 'Counts daily messages',
-  unixExpression: '0 0 0 * * 2-6',
-  file: './snapshot.daily',nextRun: new Date(1442718000233)
-}, {
-  name: 'weeklyMessages',
-  description: 'Counts weekly messages',
-  unixExpression: '0 0 0 * * 6',
-  file: './snapshot.daily'
-}, {
-  name: 'monthlyMessages',
-  description: 'Counts monthly messages',
-  unixExpression: '0 0 0 1 * *',
-  file: './snapshot.daily'
-}, {
-  name: 'yearlyMessages',
-  description: 'Counts yearly messages',
-  unixExpression: '0 0 0 1 0 *',
-  file: './snapshot.daily'
-}];
+function runCron(cron) {
+  // For now, whatever the state is, we can consider it's already running but TODO IMRPOVE
+  if (getCronStatus(cron.name))
+    return console.warn("Cron %s was already running", cron.name);
 
-
-_.each(crons, function initCron(cron) {
   var cronJob = new CronJob(cron.unixExpression, runJob, null, false, TZ, cron),
     startCronJob = _.bind(cronJob.start, cronJob),
     task = require(cron.file);
 
   function runJob(cron) {
+    setCronStatus(cron.name, 'running');
     cron = cron || this;
     return task(cron)
       .then( function updateRunDates() {
+        setCronStatus(cron.name, 'waiting');
         cron.lastRun = new Date();
         cron.nextRun = cronJob.cronTime.sendAt().toDate();
+
+        if (_.isFunction(cron.save)) cron.save();
+
         console.log('%s finished on %s. Will run again on %s', cron.name, cron.lastRun, cron.nextRun);
       })
       .fail( function handleError(err) {
@@ -56,14 +54,38 @@ _.each(crons, function initCron(cron) {
       .then(startCronJob);
   } else {
     startCronJob();
+    setCronStatus(cron.name, 'waiting');
     cron.nextRun = cronJob.cronTime.sendAt().toDate();
+
+    if (_.isFunction(cron.save)) cron.save();
+
     console.log('%s will run on %s', cron.name, cron.nextRun);
   }
-});
+}
 
 
 
+function runExistentCrons() {
+  Cron.findQ()
+    .then(function runCrons(crons) {
+      _.each(crons, runCron);
+    })
+    .fail(function logError(err) {
+      console.error(err);
+    });
+}
+
+// TODO IMPROVE isolate this global var
+var cronStatus = {};
+function getCronStatus(cronName) {
+  return cronStatus[cronName];
+}
+function setCronStatus(cronName, status) {
+  cronStatus[cronName] = status;
+}
 
 exports = module.exports = function (_app) {
   app = _app;
+  cronTab.runExistentCrons();
+  return cronTab;
 };
