28ead4909ae38dde2516599dd5744ce11387a8aa
sebastian.bogado <sebastian.j.bogado@gmail.com>
2015-09-21 10:05:08 -0300

    Ranking snapshots: fix cron socket breaking change

--- server/api/cron/cron.socket.js
+++ server/api/cron/cron.socket.js
@@ -6,14 +6,14 @@
 
 var Cron = require('./cron.model');
 
-//exports.register = function(socket) {
+exports.register = function(socket) {
 //  Cron.schema.post('save', function (doc) {
 //    onSave(socket, doc);
 //  });
 //  Cron.schema.post('remove', function (doc) {
 //    onRemove(socket, doc);
 //  });
-//};
+};
 
 function onSave(socket, doc, cb) {
   socket.emit('cron:save', doc);
